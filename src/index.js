800256fd1978455cea0469d6e3c76424867f399c
Srim <deamoon0801@naver.com>
2021-06-07 22:16:30 +0900

    [ADD] redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./assets/style/index.scss"
+import "./assets/style/index.scss";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddlerware from "redux-promise";
+import reduxThunk from "redux-thunk";
+import { rootReducer } from "./store/modules/index";
+
+const createStoreWidthMiddleware = applyMiddleware(promiseMiddlerware, reduxThunk)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWidthMiddleware(
+        rootReducer,
+        //개발자 도구를 사용하기 위해 설정
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
