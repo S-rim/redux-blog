0a4aa3c44416d46145a83d3be227f943a777ec8d
Srim <deamoon0801@naver.com>
2021-06-21 11:44:17 +0900

    [ADD] <AuthContainer> error exception

--- src/containers/AuthContainer/LoginContainer.js
+++ src/containers/AuthContainer/LoginContainer.js
@@ -3,6 +3,7 @@ import { Login } from "../../components";
 import useChangeInput from "../../lib/hooks/useChangeInputs";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../store/modules/user/actions";
+import { SuccessToast, ErrorToast } from "../../lib/toast";
 
 const LoginContainer = () => {
   const dispatch = useDispatch();
@@ -11,10 +12,23 @@ const LoginContainer = () => {
   const onSubmitLogin = () => {
     dispatch(loginUser(loginData))
       .then(() => {
-        alert("로그인이 정상적으로 완료되었습니다");
+        SuccessToast("로그인이 정상적으로 완료되었습니다.");
       })
       .catch((err) => {
-        console.log(err);
+        switch (err.response.status) {
+          case 401:
+            ErrorToast("비밀번호가 틀렸습니다.");
+            break;
+          case 404:
+            ErrorToast("없는 계정입니다. 회원가입을 해주세요.");
+            break;
+          case 500:
+            ErrorToast("서버 에러입니다.");
+            break;
+          default:
+            ErrorToast("에러가 발생했습니다. 다시 시도하세요.");
+            break;
+        }
       });
   };
   return (
--- src/containers/AuthContainer/SignUpContainer.js
+++ src/containers/AuthContainer/SignUpContainer.js
@@ -3,6 +3,7 @@ import { SignUp } from "../../components";
 import useChangeInput from "../../lib/hooks/useChangeInputs";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../../store/modules/user/actions";
+import { ErrorToast, SuccessToast } from "../../lib/toast";
 
 const SignUpContainer = () => {
   const dispatch = useDispatch();
@@ -11,10 +12,20 @@ const SignUpContainer = () => {
   const onSubmitRegister = () => {
     dispatch(registerUser(registerData))
       .then(() => {
-        alert("가입이 정상적으로 완료되었습니다");
+        SuccessToast("가입이 정상적으로 완료되었습니다");
       })
       .catch((err) => {
-        console.log(err);
+        switch (err.response.status) {
+          case 409:
+            ErrorToast("이미 있는 계정입니다.");
+            break;
+          case 500:
+            ErrorToast("서버 에러입니다.");
+            break;
+          default:
+            ErrorToast("에러가 발생했습니다. 다시 시도하세요.");
+            break;
+        }
       });
   };
 
