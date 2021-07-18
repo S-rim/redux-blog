db16d772aa7ed6fdd69a362b4da5d7e177944e96
Srim <deamoon0801@naver.com>
2021-07-18 22:04:56 +0900

    [ADD] <MainPost> post list

--- src/components/MainPost/PostItem.js
+++ src/components/MainPost/PostItem.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const PostItem = () => {
+const PostItem = ({ post, deletePost }) => {
+  const { id, title, content } = post;
+
   return (
     <article className="post-item">
-      <h1>글 제목어쩌구</h1>
-      <p className="post-content">글내용</p>
+      <h1>{title}</h1>
+      <p className="post-content">{content}</p>
       <div className="post-info">
         <p className="post-info-date">2020-05-29</p>
         <div className="post-update">
-          <p className="post-update-button">수정</p>
-          <p>삭제</p>
+          <Link
+            to={{
+              pathname: `/edit/${id}`,
+              state: id,
+            }}
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <p className="post-update-button">수정</p>
+          </Link>
+
+          <p
+            onClick={() => {
+              deletePost(id);
+            }}
+          >
+            삭제
+          </p>
         </div>
       </div>
     </article>
--- src/components/MainPost/PostMain.js
+++ src/components/MainPost/PostMain.js
@@ -2,19 +2,29 @@ import React from "react";
 import { PageTemplate } from "../Common";
 import PostItem from "./PostItem";
 
-const PostMain = () => {
+const PostMain = ({ prevPage, nextPage, page, posts, deletePost }) => {
   return (
     <PageTemplate>
       <section className="post-section">
-        <PostItem />
-        <PostItem />
-        <PostItem />
+        {posts && !(typeof post == "string") ? (
+          <>
+            {posts.map((post) => {
+              return <PostItem key={post.id} post={post} deletePost={deletePost} />;
+            })}
+          </>
+        ) : (
+          <h1>추천 목록을 불러오는 데에 실패했습니다. 다시 시도하세요.</h1>
+        )}
       </section>
       <section className="page">
         <div className="page-wrapper">
-          <div className="page-item">{"<"}</div>
-          <div className="page-item color">1</div>
-          <div className="page-item">{">"}</div>
+          <div className="page-item" onClick={prevPage}>
+            {"<"}
+          </div>
+          <div className="page-item color">{page + 1}</div>
+          <div className="page-item" onClick={nextPage}>
+            {">"}
+          </div>
         </div>
       </section>
     </PageTemplate>
