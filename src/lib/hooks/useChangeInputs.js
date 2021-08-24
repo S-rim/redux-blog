f2f56407ae187b49fc3c405fa19c759074b216b5
Srim <deamoon0801@naver.com>
2021-08-24 18:20:56 +0900

    [MODIFY] <lib/hooks> useChangeInputs

--- src/lib/hooks/useChangeInputs.js
+++ src/lib/hooks/useChangeInputs.js
@@ -3,13 +3,10 @@ import { useState, useCallback } from "react";
 function useChangeInput(initalValue) {
   const [data, setData] = useState(initalValue);
 
-  const handler = useCallback(
-    (e) => {
-      const { value, name } = e.target;
-      setData((prev) => ({ ...prev, [name]: value }));
-    },
-    [data]
-  );
+  const handler = useCallback((e) => {
+    const { value, name } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }, []);
   return [data, handler];
 }
 
