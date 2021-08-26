896505cd094527119cf09a89397fecbc58d85aa5
Srim <deamoon0801@naver.com>
2021-08-26 18:22:53 +0900

    [MODIFY] AddPost history & catch

--- src/containers/PostContainer/AddPostContainer.js
+++ src/containers/PostContainer/AddPostContainer.js
@@ -4,18 +4,21 @@ import useChangeInput from "../../lib/hooks/useChangeInputs";
 import { useDispatch } from "react-redux";
 import { writePost } from "../../store/modules/post/actions";
 import { SuccessToast, ErrorToast } from "../../lib/toast";
+import { useHistory } from "react-router-dom";
 
 const AddPostContainer = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const [postData, setPostData] = useChangeInput({ title: "", content: "" });
 
   const onAddPost = () => {
     dispatch(writePost(postData))
       .then(() => {
         SuccessToast("글 쓰기가 정상적으로 완료되었습니다");
+        history.push("/post")
       })
       .catch((err) => {
-        ErrorToast(err);
+        ErrorToast(err.response.data.message);
       });
   };
 
